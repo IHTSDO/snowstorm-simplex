8fbb6a02a1865bc7f3a37464efe153ada683250f
Kai Kewley <kaikewley@gmail.com>
2026-03-04 21:57:48 +0000

    ISTO-148 Add CORS allow any origin by default.
    Disable using snowstorm.rest-api.allowAnyOrigin=false

--- src/main/java/org/snomed/snowstorm/fhir/config/HapiRestfulServlet.java
+++ src/main/java/org/snomed/snowstorm/fhir/config/HapiRestfulServlet.java
@@ -5,11 +5,13 @@ import ca.uhn.fhir.parser.LenientErrorHandler;
 import ca.uhn.fhir.parser.StrictErrorHandler;
 import ca.uhn.fhir.rest.api.EncodingEnum;
 import ca.uhn.fhir.rest.server.RestfulServer;
+import ca.uhn.fhir.rest.server.interceptor.CorsInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snomed.snowstorm.fhir.services.*;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.cors.CorsConfiguration;
 
 public class HapiRestfulServlet extends RestfulServer {
 
